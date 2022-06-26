b71c7755a3040676a31e6cee5250954de1578436
eric_dp <623025758@qq.com>
2022-06-27 00:25:59 +0800

    spark-sql

--- spark/src/main/java/Test/SparkSQLTest3.java
+++ spark/src/main/java/Test/SparkSQLTest3.java
@@ -7,9 +7,7 @@ package Test;
  */
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
+import org.apache.spark.sql.*;
 
 
 import org.apache.spark.sql.SparkSession;
@@ -35,16 +33,27 @@ public class SparkSQLTest3 {
                 .config("spark.some.config.option", "some-value")
                 .getOrCreate();
 
+        spark.conf().set("spark.sql.planChangeLog.level","WARN");
+
         Dataset<Row> jdbcDF = spark.read()
                 .format("jdbc")
                 .option("url", "jdbc:mysql://127.0.0.1:3306/test")
-                .option("dbtable", "(SELECT * FROM city) city")
+                .option("dbtable", "city")
                 .option("user", "root")
                 .option("password", "root")
                 .load();
+        jdbcDF.show();
+
+
+            jdbcDF.registerTempTable("city");
+        jdbcDF.registerTempTable("city2");
+          //  spark.sql("select NAME,CODE from (select NAME,CODE from city where 1 = 1 and CODE>=101010100) tmp where tmp.CODE<=20000000 ").explain(true);
+
+        spark.sql("SELECT DISTINCT a.NAME FROM (" +
+                "SELECT*FROM city WHERE CODE> 101010100 AND 1=1) a WHERE a.id=2 except " +
+                "SELECT NAME FROM city2 WHERE CODE> 101280101 ").explain(true);
 
         jdbcDF.printSchema();
-        jdbcDF.show();
 
         spark.stop();
     }
